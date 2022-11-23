5c4f4c9a36310d39baff34151f87f6825484478d
kimscastle <uisong26559287@gmail.com>
2022-11-23 20:29:22 +0900

    :lipstick: DeviceInfo 추가

---
+++ LottoDiary/LottoDiary/Sources/Utility/DeviceInfo/DeviceInfo.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceInfo.swift
+//  LottoDiary
+//
+//  Created by uiskim on 2022/11/23.
+//
+
+import UIKit
+
+struct DeviceInfo {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
