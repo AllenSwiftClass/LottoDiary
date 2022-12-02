f460005e0442c8208ba1cf8d38647e8d22b57ba6
SunnnySong <tjsghktjswls@naver.com>
2022-12-02 17:23:33 +0900

    💄 CustomTabBar UI구현

---
+++ LottoDiary/LottoDiary/Sources/Present/RandomLottoNumberViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RandomLottoNumberViewController.swift
+//  LottoDiary
+//
+//  Created by 송선화 on 2022/12/02.
+//
+
+import UIKit
+
+final class RandomLottoNumberViewController: UIViewController {
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      view.backgroundColor = .blue
+  }
+}
