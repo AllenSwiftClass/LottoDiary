23345519ae6350ccc12a30756c48f0483fa121d2
kimscastle <uisong26559287@gmail.com>
2022-12-06 19:07:27 +0900

    :tada: 로또 번호 추첨 Tap 프로젝트 생성

---
+++ LottoDiary/LottoDiary/Sources/Present/NumberDrawTap/Views/LottoBall.swift
@@ -0,0 +1,20 @@
+//
+//  LottoBall.swift
+//  LottoDiary
+//
+//  Created by uiskim on 2022/12/06.
+//
+
+import UIKit
+
+class LottoBall: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ LottoDiary/LottoDiary/Sources/Present/NumberDrawTap/Views/LottoNumbersView.swift
@@ -0,0 +1,20 @@
+//
+//  LottoNumbersView.swift
+//  LottoDiary
+//
+//  Created by uiskim on 2022/12/06.
+//
+
+import UIKit
+
+class LottoNumbersView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
