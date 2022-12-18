c4be9e2b3d11370347794f10b09a296e4207d098
kimscastle <uisong26559287@gmail.com>
2022-12-18 19:54:40 +0900

    :tada: homeviewcontroller 프로젝트 시작

---
+++ LottoDiary/LottoDiary/Sources/Present/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  LottoDiary
+//
+//  Created by uiskim on 2022/11/23.
+//
+
+import UIKit
+import SnapKit
+
+final class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ LottoDiary/LottoDiary/Sources/Present/Home/Models/DummyUser.swift
@@ -0,0 +1,14 @@
+//
+//  DummyUser.swift
+//  LottoDiary
+//
+//  Created by uiskim on 2022/12/18.
+//
+
+import Foundation
+
+struct DummyUser {
+    let nickName: String
+}
+
+let testUser = DummyUser(nickName: "Brody")
